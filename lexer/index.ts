0c812116c1add7f33639338154aa58a1ea4affb1
David Massana <00072416@uca.edu.sv>
2022-10-31 23:20:55 -0600

    todos

--- lexer/index.ts
+++ lexer/index.ts
@@ -2,3 +2,8 @@ export * from './lexer';
 export * from './machines';
 export * from './structs';
 export * from './lang';
+
+// TODO: DFA comments
+// TODO: DFA Operators rev
+// TODO: Docs rev
+// TODO: Symbols Table
